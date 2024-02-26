8766db4c6d10967f4cecbe95b60d5b9cafbe5c27
Tyler Milner <tylermilner@gmail.com>
2024-02-26 02:19:18 -0600

    Automatically open the generated Xcode project after running Cookiecutter.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,6 +9,11 @@ def remove_xcodegen_yml() -> None:
     print("Removing XcodeGen `project.yml` file...")
     os.remove("project.yml")
 
+def open_xcode_project() -> None:
+    print("Opening Xcode project...")
+    xcode_project = "{{ cookiecutter.__project_name_no_spaces }}"
+    subprocess.run(["open", f"{xcode_project}.xcodeproj"])
+
 if __name__ == "__main__":
     # Generate the Xcode project using XcodeGen
     run_xcodegen()
@@ -18,5 +23,9 @@ if __name__ == "__main__":
 
     # Remove XcodeGen `project.yml` file now that the Xcode project has been generated
     remove_xcodegen_yml()
+
+    # Open the generated Xcode project
+    if "{{ cookiecutter.open_xcode_project }}":
+        open_xcode_project()
 
     print("Post-gen script complete")
