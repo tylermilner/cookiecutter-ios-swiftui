338e6efd7282c069da6074dd5e627ca0e507e1e9
Tyler Milner <tylermilner@gmail.com>
2024-02-26 01:10:11 -0600

    Cleanup XcodeGen `project.yml` after project generation.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,13 @@
-import sys
+import os
 import subprocess
 
 def run_xcodegen() -> None:
     print("Running XcodeGen...")
     subprocess.run(["xcodegen"], check=True)
+
+def remove_xcodegen_yml() -> None:
+    print("Removing XcodeGen `project.yml` file...")
+    os.remove("project.yml")
 
 if __name__ == "__main__":
     # Generate the Xcode project using XcodeGen
@@ -11,6 +15,8 @@ if __name__ == "__main__":
 
     # TODO: Run tests using Fastlane
     # TODO: Initialize git repo (see example: https://github.com/riteshhgupta/swift-cookiecutter/blob/master/cookiecutter/hooks/post_gen_project.sh)
-    # TODO: Remove XcodeGen `project.yml` file
+
+    # Remove XcodeGen `project.yml` file now that the Xcode project has been generated
+    remove_xcodegen_yml()
 
     print("Post-gen script complete")
