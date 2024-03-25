599a8fb0beec53834d79be7484f2618b54000d03
Tyler Milner <tylermilner@gmail.com>
2024-03-24 20:28:22 -0500

    Make function that checks Swift files for text more generic.

--- tests/test_template.py
+++ tests/test_template.py
@@ -111,13 +111,13 @@ def test_project_name_replaced(baked_cookies):
         assert "# My App" in readme
 
 # Helper function to check Swift source files for header comment with target name
-def check_swift_files_for_header_comment_pattern(source_directory, pattern, line_number):
+def check_swift_files_for_text(source_directory, pattern, line_number):
     for root, dirs, files in os.walk(source_directory):
         for file in files:
             if file.endswith(".swift"):
                 with open(os.path.join(root, file), 'r') as swift_file:
-                    lines = [next(swift_file) for x in range(6)]
-                    assert re.match(pattern, lines[line_number].strip()), f"File {file} does not have expected header comment pattern"
+                    lines = [next(swift_file) for x in range(line_number + 1)]
+                    assert re.match(pattern, lines[line_number].strip()), f"File {file} does not have expected text pattern {pattern} on line {line_number}"
 
 # Test that target_name is replaced correctly in all necessary files
 def test_target_name_replaced(baked_cookies):
@@ -154,7 +154,8 @@ def test_target_name_replaced(baked_cookies):
     # Verify target name header comment in Swift source files for each target
     targets = [app_target_name, app_tests_target_name, app_uitests_target_name]
     for target in targets:
-        check_swift_files_for_header_comment_pattern(os.path.join(project_path, target), f"//  {target}", 2)
+        target_directory = os.path.join(project_path, target)
+        check_swift_files_for_text(target_directory, f"//  {target}", 2)
 
     # Verify remaining header comments for main app source file
     with open(os.path.join(project_path, app_target_name, f"{app_target_name}App.swift")) as file:
@@ -200,7 +201,7 @@ def test_full_name_replaced(baked_cookies):
     project_path = baked_cookies.project_path
 
     # Assert
-    check_swift_files_for_header_comment_pattern(project_path, f"//  Created by {full_name} on .*", 4)
+    check_swift_files_for_text(project_path, f"//  Created by {full_name} on .*", 4)
 
 def test_date_replaced(cookies):
     # Arrange
@@ -211,7 +212,7 @@ def test_date_replaced(cookies):
 
     # Assert
     project_path = result.project_path
-    check_swift_files_for_header_comment_pattern(project_path, f"//  Created by .* on {date}", 4)
+    check_swift_files_for_text(project_path, f"//  Created by .* on {date}", 4)
 
 def test_remove_xcodegen_yml(cookies):
     # Act
