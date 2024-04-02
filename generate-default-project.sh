28b48bc77070e7af12d505fb449a8c5548829c23
Tyler Milner <tylermilner@gmail.com>
2024-04-02 01:55:02 -0500

    Don't open Xcode by default in `generate-default-project` script. Allow override to open Xcode by providing the `open-xcode` parameter. Fixes #23.

--- generate-default-project.sh
+++ generate-default-project.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Shell script to generate a project with the cookiecutter default values
-# Usage: ./generate-default-project.sh
+# Shell script to generate a project with the cookiecutter default values for quick debugging during development.
+# Technically strays from the Cookiecutter project defaults slightly:
+#   - No git repository will be created.
+#   - Xcode will not be opened (use 'open-xcode' to override).
+# Usage: ./generate-default-project.sh [open-xcode]
+# open-xcode: Optional. If present, Xcode will be opened after generating the template.
+
+# Get the command line argument
+open_xcode=$1
+
+# Set the open_xcode_project value based on the open_xcode argument
+if [ "$open_xcode" = "open-xcode" ]; then
+    open_xcode_project=True
+else
+    open_xcode_project=False
+fi
 
 # Generate a project with the cookiecutter default values
-pipenv run cookiecutter . --no-input --overwrite-if-exists initialize_git_repo=False
+pipenv run cookiecutter . --no-input --overwrite-if-exists initialize_git_repo=False open_xcode_project=$open_xcode_project
