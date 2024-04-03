f4e7aaacdeb773314911fe59109872aa69f05e0b
Tyler Milner <tylermilner@gmail.com>
2024-04-02 21:55:24 -0500

    Simplify run-tests script based on Jon Reid's example in his "TDD in SwiftUI" series - https://qualitycoding.org/github-actions-ci-xcode/#t-1711330490960.

--- {{ cookiecutter.project_root }}/run-tests.sh
+++ {{ cookiecutter.project_root }}/run-tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-xcodebuild test -project {{ cookiecutter.__project_name_no_spaces }}.xcodeproj -scheme {{ cookiecutter.__project_name_no_spaces }} -destination 'platform=iOS Simulator,name=iPhone 15 Pro,OS=latest'
+SCHEME='{{ cookiecutter.__project_name_no_spaces }}'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+
+xcodebuild test -scheme $SCHEME -destination "$DESTINATION"
