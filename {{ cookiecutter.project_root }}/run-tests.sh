8d514d00ad5e83b80f2cb8a59282ff8a9af0df96
Tyler Milner <tylermilner@gmail.com>
2024-04-28 22:36:19 -0500

    Fix linter error about missing double quotes in bash script.

--- {{ cookiecutter.project_root }}/run-tests.sh
+++ {{ cookiecutter.project_root }}/run-tests.sh
@@ -3,4 +3,4 @@
 SCHEME='{{ cookiecutter.__project_name_no_spaces }}'
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
 
-xcodebuild test -scheme $SCHEME -destination "$DESTINATION"
+xcodebuild test -scheme "$SCHEME" -destination "$DESTINATION"
